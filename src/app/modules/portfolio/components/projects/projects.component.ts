f1a82052eced91b8bfa438fa8b30292fe3376f7b
Dener Troquatte <you@example.com>
2023-12-05 18:52:02 -0300

    Curso de Angular

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -30,11 +30,26 @@ export class ProjectsComponent {
       width: '100px',
       height: '51px',
       description:
-        'Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript. Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.',
+        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
       links: [
         {
           name: 'Conheça o Blog',
           href: 'https://vidafullstack.com.br/',
+        },
+      ],
+    },
+    {
+      imgSrc: 'assets/img/projects/lista-de-tarefas.png',
+      imgAlt: 'Lista de Tarefas',
+      title: 'Lista de Tarefas',
+      width: '84px',
+      height: '86px',
+      description:
+        '<p>Prepare-se para uma jornada empolgante de aprendizado e aperfeiçoamento, onde cada tarefa proposta representa um passo significativo em direção à maestria no desenvolvimento web.</p> <p>Estamos aqui para desbravar novos horizontes juntos e construir soluções inovadoras.</p> <p>Vamos começar essa emocionante jornada de conquistas e descobertas!</p>',
+      links: [
+        {
+          name: 'Conheça o projeto',
+          href: 'https://troquatte.github.io/curso-angular-lista-de-tarefas/browser/',
         },
       ],
     },
